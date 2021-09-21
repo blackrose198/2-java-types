578810f7ce6d0445398c5ffb49b0386a6f9f9cab
blackrose198 <grushinskayan@gmail.com>
2021-09-21 15:20:00 +0300

    fix task3

--- task03/src/com/example/task03/Task03.java
+++ task03/src/com/example/task03/Task03.java
@@ -5,17 +5,21 @@ public class Task03 {
     public static int getMetreFromCentimetre(int centimetre) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        if (centimetre >= 100) {
+            centimetre = centimetre /100;
+            return centimetre;
+        }
+        else
+            return 0;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = getMetreFromCentimetre(345);
         System.out.println(result);
-         */
+
     }
 
 }
