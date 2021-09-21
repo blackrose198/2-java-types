d16c6edb82c20ed704ccbee3c0ae3a47b2028062
blackrose198 <grushinskayan@gmail.com>
2021-09-21 15:49:10 +0300

    fix task9

--- task09/src/com/example/task09/Task09.java
+++ task09/src/com/example/task09/Task09.java
@@ -8,16 +8,15 @@ public class Task09 {
         float a = 1.0f;
         float b = 3.0f;
         float c = 1.0e9f;
-        double x = (a / b - 1.0 / 3.0) * c;
 
-        return x;
+        return ((double)a / (double)b - 1.0 / 3.0) * (double)c;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение
-        /*
+
         System.out.println(solution() == 0.0d);
-        */
+
     }
 
 }
