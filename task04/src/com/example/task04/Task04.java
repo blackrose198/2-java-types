f561df42543c90202a29ab87395151e88b430122
blackrose198 <grushinskayan@gmail.com>
2021-09-21 15:24:14 +0300

    fix task4

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -5,17 +5,28 @@ public class Task04 {
     public static float calculate(int a, int b, String operation) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        char operation_ = operation.charAt(0);
+        float ans = 0;
+        switch(operation_) {
+            case '+': ans = a + b;
+                break;
+            case '-': ans = a - b;
+                break;
+            case '*': ans = a * b;
+                break;
+            case '/': ans = (float) (a) / (float) (b);
+                break;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
+
     }
 
 }
